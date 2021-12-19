348c58bbc3fee2e35b00d46e3a7e718d80b8b0c2
Boguslaw Fries <boguslaw.fries@autopartner.com>
2021-12-19 14:43:32 +0100

    adj

--- CalculationLogic/Pricelist+2020-01-01/elements/AttributeAdjPct.groovy
+++ CalculationLogic/Pricelist+2020-01-01/elements/AttributeAdjPct.groovy
@@ -1,6 +1,6 @@
-def attributeAdjPct = PriceListLib.attributeAdj()
-//def productLifeCycle = api.product("ProductLifeCycle")
-//def attributeAdjPct = api.vLookup("AttributeAdj", "AttributeAdj", productLifeCycle)
+//def attributeAdjPct = PriceListLib.attributeAdj()
+def productLifeCycle = api.product("ProductLifeCycle")
+def attributeAdjPct = api.vLookup("AttributeAdj", "AttributeAdj", productLifeCycle)
 
 if(attributeAdjPct == null) {
     api.addWarning("Unable to look up Attribute Adjustment with the Product Life Cycle")
